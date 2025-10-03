a634421d3bb9eb63fc2b47cd27cfdb2007ef1b3f
molas34 <nojus.mozuraitis@gmail.com>
2025-10-03 18:30:11 +0300

    10:03

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainTeleOp.java
@@ -4,16 +4,17 @@ import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.hardware.HardwareDevice;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-
+@TeleOp()
 public class MainTeleOp extends LinearOpMode {
     Motor kP, kG, dP, dG; //kairė priekis/galas, desinė priekis/galas
     Double sp = 0.5; //Greitis
-    CRServo P1S, P2S; // Padavimo servai
-    DcMotor pm, sm; //Paėmimas, išmetimas
+   // CRServo P1S, P2S; // Padavimo servai
+   // DcMotor pm, sm; //Paėmimas, išmetimas
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -23,14 +24,14 @@ public class MainTeleOp extends LinearOpMode {
         dP = new Motor(hardwareMap, "dP", Motor.GoBILDA.RPM_312); // 2 lizdas control hub
         dG = new Motor(hardwareMap, "dG", Motor.GoBILDA.RPM_312); // 3 lizdas control hub
 
-        //Išmetimas/Paėmimas
+     /*   //Išmetimas/Paėmimas
         pm = hardwareMap.get(DcMotor.class, "pm");
         sm = hardwareMap.get(DcMotor.class, "sm");
 
         //Padavimo servai
         P1S = hardwareMap.get(CRServo.class, "P1S");
         P2S = hardwareMap.get(CRServo.class, "P2S");
-
+*/
         waitForStart();
         while (!isStopRequested()) {
 
@@ -39,11 +40,11 @@ public class MainTeleOp extends LinearOpMode {
 
            drive.driveFieldCentric(
                    gamepad1.left_stick_x * sp, // strafe/drift
-                   gamepad1.left_stick_y * sp, // priekis
+                   gamepad1.left_stick_y * -sp, // priekis
                    gamepad1.right_stick_x * sp, // posūkis
                    0
            );
-
+/*
            //Šaudymas:
             //išmetimas
            if (gamepad1.triangle){
@@ -63,7 +64,7 @@ public class MainTeleOp extends LinearOpMode {
             if (0!=0) {
 
 
-            }
+            }*/
 
         }
 
