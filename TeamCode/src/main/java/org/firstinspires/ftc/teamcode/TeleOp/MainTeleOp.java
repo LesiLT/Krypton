4f978b838afc3fa174e88ff2db5434de0afdb757
molas34 <nojus.mozuraitis@gmail.com>
2025-10-10 17:05:13 +0300

    10 10

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainTeleOp.java
@@ -16,7 +16,7 @@ public class MainTeleOp extends LinearOpMode {
     Motor kP, kG, dP, dG; //kairė priekis/galas, desinė priekis/galas
     Double sp = 0.5; //Greitis
 
-    DcMotor pm, sm; //Paėmimas, išmetimas //0, 1 expansion hub
+    DcMotor pm, sm1,sm2; //Paėmimas, išmetimas //0, 1, 2expansion hub
 
 
     boolean prev = false;
@@ -32,8 +32,10 @@ public class MainTeleOp extends LinearOpMode {
 
         //Išmetimas/Paėmimas
         pm = hardwareMap.get(DcMotor.class, "pm");
-        sm = hardwareMap.get(DcMotor.class, "sm");
-        sm.setDirection(DcMotorSimple.Direction.REVERSE);
+        sm1 = hardwareMap.get(DcMotor.class, "sm1");
+        sm2 = hardwareMap.get(DcMotor.class, "sm2");
+        sm1.setDirection(DcMotorSimple.Direction.REVERSE);
+        sm2.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
         waitForStart();
@@ -58,6 +60,12 @@ public class MainTeleOp extends LinearOpMode {
             pm.setPower(0);
 
             //išmetimas
+            /*telemetry.addData("Titas == blogas",gamepad1.right_trigger);
+            if(gamepad1.circle)telemetry.update();
+            sm1.setPower(-gamepad1.right_trigger);
+            sm2.setPower(gamepad1.right_trigger);*/
+
+
             boolean paspaustas = false;
             paspaustas = gamepad1.triangle;
             if(paspaustas && !prev)
@@ -67,9 +75,11 @@ public class MainTeleOp extends LinearOpMode {
             prev = paspaustas;
             if(motorOn){
                 gamepad1.rumble(500, 500, 600);
-                sm.setPower(-1);
+                sm1.setPower(-1);
+                sm2.setPower(1);
             }
-            else sm.setPower(0);
+            else{ sm1.setPower(0);
+                sm2.setPower(0);}
 
         }
 
